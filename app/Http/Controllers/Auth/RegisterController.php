01a89af23340e1fa22244c7d14245031d99670c4
Azwel Simwinga <mulumbesimwinga@gmail.com>
2021-10-21 15:13:34 +0200

    added man number to user details

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            //'phone' => ['required', 'numeric', 'min:11'],
+            'manNumber' => ['required', 'numeric', 'max:9999'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -65,8 +67,9 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
+            'name'=> $data['name'],
+            'manNumber'=> $data['manNumber'],
+            'email' => $data['email'],            
             'password' => Hash::make($data['password']),
         ]);
     }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,9 +25,22 @@
                             </div>
                         </div>
 
+                        
+                        <div class="form-group row">
+                            <label for="manNumber" class="col-md-4 col-form-label text-md-right">{{ __('Man Number') }}</label>
+                            <div class="col-md-6">
+                                <input id="manNumber" type="number" class="form-control @error('manNumber') is-invalid @enderror" name="manNumber" value="{{ old('manNumber') }}" required autocomplete="name" autofocus>
+
+                                @error('manNumber')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
