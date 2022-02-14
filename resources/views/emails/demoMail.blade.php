1db28ed0e294f4b63ecf91067878edaf0a72a055
Azwel Simwinga <mulumbesimwinga@gmail.com>
2022-02-14 16:52:24 +0200

    `Ti Dwale?

--- resources/views/emails/demoMail.blade.php
+++ resources/views/emails/demoMail.blade.php
@@ -7,6 +7,6 @@
         <h1>Query : {{$data['categoryDetails']}}</h1>
         <h2>{{$data['subCategoryDetails']}}</h2>
         <h3>{{$data['queryDetails']}}</h3> 
-        <h3>From {{ Auth::user()->email }}</h3>
+        <h3>From <b>{{ Auth::user()->name }}</b></h3>
     </body>
 </html> 
